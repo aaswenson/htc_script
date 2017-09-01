6e32726a4017fdc66a4d9914c3099e0e753cd4a4
Mouginot B <mouginot.baptiste@gmail.com>
2017-09-01 14:46:12 -0500

    update to new script

---
+++ mure_run_tar_send.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+line=$(cat $1)
+# Setup
+source load.bash
+source ${home}/std_setup.bash
+source ${home}/install_mcnp_mure_data_PSPEC.bash
+cd TRU_MOX_SOFT
+
+# compilation
+g++  -o MOX MOX.cc -I$MURE_include -I$MURE_ExternalPkg -L$MURE_lib -lMUREpkg -lvalerr -lmctal -fopenmp
+
+# run
+./MOX $line
+
+# taring
+tar -zcf ${1}.tar.gz U5*.dat U5*.log U5*.info
+rm U5*.dat U5*.log U5*.info
+tar -zcf ${1}_raw.tar.gz U5*
+
+# sending to box
+curl -1 -v --disable-epsv --ftp-skip-pasv-ip --ftp-ssl --upload-file ${1}_raw.tar.gz  -netrc ftp://ftp.box.com/MODEL_DATA/PWR_TRU_MOX_PSPEC/${1}_raw.tar.gz
+
+
+# backup the rest
+mv ${1}.tar.gz ${home}/
+cd ${home}
+rm -rf *.bash tmp
