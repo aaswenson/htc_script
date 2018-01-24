c4507688356c8337e0e518220ee0da389955bd8b
Alexander Swenson <aaswenson@wisc.edu>
2018-01-24 16:07:33 -0600

    remove all uncessary files

--- LHS_to_file.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-iter=$((0))
-
-cat LHS | while read line; do
-    echo $line > input/${iter}
-    iter=$(( $iter +1 ))
-done
-ls input > input_list.txt
--- geom_install.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source load.bash
-echo "load done"
-source ${home}/std_setup.bash
-echo "std_setup done"
-source ${home}/install_mcnp_mure_data.bash
-
